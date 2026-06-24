c8a8981b38085dde55c132bff293e946fa54d8a1
arvinhakoupian <75630790+arvinhakoupian@users.noreply.github.com>
2026-06-24 14:50:31 -0700

    first

---
+++ components/header/Header.tsx
@@ -0,0 +1,45 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useAuth} from "../../contexts/AuthContext";
+import {Button} from "../Button";
+
+export function Header() {
+    const {isAuthenticated, logout} = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+      logout();
+      navigate("/");  
+    };
+    return (
+        <header className="flex items-center justify-between bg-white p-4 shadow"> 
+            <h2 className="text-xl font-bold">Event app</h2>
+            <nav className="flex items-center gap-4">
+                <ul className="flex gap-4">
+                    <li>
+                        <Link to="/">All Events</Link>
+                    </li>
+                    <li>
+                        <Link to="/my-events">My Events</Link>
+                    </li>
+                    {isAuthenticated ? (
+                    <li>
+                        <Button variant="secondary" size="small" onClick={handleLogout}>
+                            Logout
+                        </Button>
+                    </li>
+                       ) : (
+                       <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                       </>
+                    )}
+
+                </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom";
+import {EventsPage} from "../pages/EventsPage";
+import {MyEventsPage} from "../pages/MyEventsPage";
+import {LoginPage} from "../pages/LoginPage";
+import {RegisterPage} from "../pages/RegisterPage";
+import {EventDetailsPage} from "../pages/EventDetailsPage";
+import {EditEventsPage} from "../pages/EditEventsPage";
+import {Header} from "../components/header/Header";
+import {AuthProvider} from "../contexts/AuthProvider";
+import {ProtectedRoute} from "../components/ProtectedRoute";
+
+function App() {
+  return (
+    <AuthProvider>
+        <div className="min-h-screen bg-gray-50">
+            <Router>
+                <Header />
+                <main className="container mx-auto p-8">
+                    <Routes>
+                        <Route path="/" element={<EventsPage />} />
+                        <Route path="/my-events" 
+                        element={
+                        <ProtectedRoute>
+                            <MyEventsPage />
+                        </ProtectedRoute>
+                    }
+                />
+                        <Route path="/events/:eventId" element={<EventDetailsPage />} />
+                        <Route path="/events/:eventId/edit"
+                         element={
+                            <ProtectedRoute>
+                                <EditEventsPage />
+                            </ProtectedRoute>
+                        }
+                    />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    </AuthProvider>
+  );
+}
+
+export default App
